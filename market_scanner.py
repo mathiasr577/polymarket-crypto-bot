e439d2321cf1ba5ee53d281fb18908f9d718f715
mathiasr577 <mathiasr577@gmail.com>
2026-06-08 15:02:24 -0400

    fix: datetime timezone

--- market_scanner.py
+++ market_scanner.py
@@ -97,7 +97,10 @@ class MarketScanner:
             return None
         try:
             s = s.replace("Z", "+00:00")
-            return datetime.fromisoformat(s)
+            dt = datetime.fromisoformat(s)
+            if dt.tzinfo is None:
+                dt = dt.replace(tzinfo=timezone.utc)
+            return dt
         except Exception:
             return None
 
